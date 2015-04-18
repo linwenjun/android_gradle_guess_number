8429a6d0fd120e0fac14453f687dad1ab455c01c
Wenjun LIN <wenjun.lin@rea-group.com>
2015-04-18 22:16:40 +0800

    Add the btn effect

--- src/main/java/org/hello/HelloActivity.java
+++ src/main/java/org/hello/HelloActivity.java
@@ -2,22 +2,32 @@ package org.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import org.joda.time.LocalTime;
 
 public class HelloActivity extends Activity {
+    private int count = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_layout);
+
+        final TextView mainText = (TextView)findViewById(R.id.main_text);
+        Button okButton = (Button)findViewById(R.id.ok_button);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainText.setText("" + (++count));
+            }
+        });
+        mainText.setText("abc");
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        LocalTime currentTime = new LocalTime();
-        TextView textView = (TextView) findViewById(R.id.text_view);
-        textView.setText("" + currentTime);
     }
 }
